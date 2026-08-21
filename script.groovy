710f0539951f692f45982151a273191673a13f8f
Felix Karg <felix.karg@adesso.de>
2026-08-21 10:20:07 +0200

    use hard coded image tags

--- script.groovy
+++ script.groovy
@@ -6,9 +6,9 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh "docker build -t felixkarg/demo-app:${IMAGE_NAME} ."
+        sh "docker build -t felixkarg/demo-app:jma-2.0 ."
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh "docker push  felixkarg/demo-app:${IMAGE_NAME}"
+        sh "docker push  felixkarg/demo-app:jma-2.0"
     }
 }
 
