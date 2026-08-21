2a1f832ce06b4cd70d160748438b4ca7eca166d2
Felix Karg <felix.karg@adesso.de>
2026-08-21 09:45:41 +0200

    branch for module 11.6

--- script.groovy
+++ script.groovy
@@ -1,14 +1,14 @@
 def buildJar() {
     echo 'building the application...'
-    sh 'mvn package'
+    sh 'mvn clean package'
 }
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanatwn/demo-app:jma-2.0 .'
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh "docker build -t felixkarg/demo-app:${IMAGE_NAME} ."
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push nanatwn/demo-app:jma-2.0'
+        sh "docker push  felixkarg/demo-app:${IMAGE_NAME}"
     }
 }
 
